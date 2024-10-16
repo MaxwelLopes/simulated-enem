db55f4cf6222845d900477bbe052aa1ac6f4d2bc
Max <maxwel.batalha@gmail.com>
2024-10-15 23:40:27 -0300

    fix middleware

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export default function middleware(request: NextRequest) {
+  console.log("Middleware foi chamado para a rota:", request.nextUrl.pathname);
+
+  if (!request.cookies.get("next-auth.session-token")) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|login|register).*)",
+  ],
+};
