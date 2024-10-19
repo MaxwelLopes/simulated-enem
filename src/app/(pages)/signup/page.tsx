27e516c8451e04f9847e1e67cdab4f1a8eaa6e7c
Hugo Vidal <vidalshugo1@gmai.com>
2024-10-18 22:51:48 -0300

    fix: Ajustado rota de registro

---
+++ src/app/(pages)/signup/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Signup from "@/app/container/Signup";
+
+const SignupPage = () => {
+  return (
+    <div>
+      <Signup />
+    </div>
+  );
+};
+
+export default SignupPage;
--- src/app/container/Signup.tsx
+++ src/app/container/Signup.tsx
@@ -1,33 +1,50 @@
 "use client";
 
-import useRegister from "../hook/userRegister";
+import { useState } from "react";
 import { useRouter } from 'next/navigation';
+import { createUser } from "@/app/service/user";
+import { signOut, useSession } from "next-auth/react";
 
-const Singup = () => {
+const Signup = () => {
   const router = useRouter();
-  const {
-    name,
-    setName,
-    email,
-    setEmail,
-    password,
-    setPassword,
-    error,
-    success,
-    handleSubmit,
-  } = useRegister();
+  const { status } = useSession();
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  if (status === "authenticated") {
+    signOut({ redirect: false });
+  }
+
+  const handleSubmitWithRedirect = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  
+    const result = await createUser(name, email, password);
+  
+    if (result?.success) {
+      setSuccess("Usuário registrado com sucesso! Você pode fazer login.");
+      setError("");
+      setTimeout(() => {
+        router.push('/login');
+      }, 500);
+    } else {
+      setError(result?.message || "Ocorreu um erro ao tentar registrar. Tente novamente.");
+      setSuccess("");
+    }
+  };
+  
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-gray-800 mb-4">Registro</h1>
       <form
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmitWithRedirect}
         className="w-full max-w-md bg-white p-8 rounded-lg shadow"
       >
         <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">
-            Nome:
-          </label>
+          <label className="block text-sm font-medium text-gray-700">Nome:</label>
           <input
             type="text"
             value={name}
@@ -37,9 +54,7 @@ const Singup = () => {
           />
         </div>
         <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">
-            Email:
-          </label>
+          <label className="block text-sm font-medium text-gray-700">Email:</label>
           <input
             type="email"
             value={email}
@@ -49,9 +64,7 @@ const Singup = () => {
           />
         </div>
         <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">
-            Senha:
-          </label>
+          <label className="block text-sm font-medium text-gray-700">Senha:</label>
           <input
             type="password"
             value={password}
@@ -61,8 +74,7 @@ const Singup = () => {
           />
         </div>
         {error && <div className="text-red-600">{error}</div>}
-        {success && <div  className="text-green-600 cursor-pointer mt-4 hover:text-green-800 hover:underline transition duration-200" onClick={() => router.push('/login')}>{success}
-        </div>}
+        {success && <div className="text-green-600">{success}</div>}
         <button
           type="submit"
           className="px-6 py-2 bg-blue-500 text-white font-semibold rounded-md shadow hover:bg-blue-600 transition duration-200"
@@ -74,4 +86,4 @@ const Singup = () => {
   );
 };
 
-export default Singup;
+export default Signup;
